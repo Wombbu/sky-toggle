d37f160dbe9e364a3da8c153d2a687126f8e4ba7
Lauri Nevanperä <lauri.nevanpera@gmail.com>
2018-12-12 23:58:35 +0200

    Clean 2

--- src/Toggler.js
+++ src/Toggler.js
@@ -3,62 +3,30 @@ import styled, { keyframes } from 'styled-components';
 
 const ANIM_DURATION = 0.5;
 
-const Switch = styled.label`
-    position: relative;
-    display: inline-block;
-    width: 120px;
-    height: ${props => props.height}px;
-    background-color: ${props => props.toggled ? '#39383d' : '#C5D5DE'};
-    border: 6px solid ${props => props.toggled ? '#1B1D1C' : '#A0B7C4'};
-    border-radius: ${props => props.height / 2 + 8}px;
-    transition: all ${ANIM_DURATION}s;
-`;
+const WIDTH = 120;
+const HEIGHT = 68;
+const BORDER_WIDTH = 6;
+const PLANET_PADDING = 4;
 
-const Input = styled.input`
-    opacity: 0;
-    width: 0;
-    height: 0;
-`;
+const colors = {
+    day: {
+        containerBorder: '#A0B7C4',
+        sky: '#C5D5DE',
+        planet: {
+            border: '#DBC85E',
+            surface: '#F2DE8A',
+        }
+    },
+    night: {
+        containerBorder: '#1B1D1C',
+        sky: '#39383d',
 
-const Slider = styled.span`
-    position: absolute;
-    cursor: pointer;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    transition: ${ANIM_DURATION}s;
-    border-radius: 50%;
-`;
-
-const Planet = styled.span`
-    position: absolute;
-    height: 60px;
-    width: 60px;
-    left: 4px;
-    bottom: 4px;
-    transition: ${ANIM_DURATION}s;
-    background-color: ${props => props.toggled ? '#FFFFFD' : '#F2DE8A'};
-    border-radius: 50%;
-    ${props => props.toggled ? 'transform: translateX(52px);' : ''}
-    border: 6px solid ${props => props.toggled ? '#E1E3D5' : '#dbc85e'};
-    box-sizing: border-box;
-    overflow: hidden;
-`;
-
-const Crater = styled.span`
-    opacity: ${props => props.toggled ? '100' : '0'};
-    width: ${props => props.size}px;
-    height: ${props => props.size}px;
-    top: ${props => props.top}px;
-    left: ${props => props.left}px;
-    border: 3px solid #E1E3D5;
-    border-radius: 50%;
-    background-color: transparent;
-    display: inline-block;
-    position: absolute;
-    transition: opacity ${ANIM_DURATION / 2}s;
-`;
+        planet: {
+            border: '#E1E3D5',
+            surface: '#FFFFFD'
+        }
+    }
+}
 
 const fadeInAnimation = keyframes`
   0% {
@@ -82,22 +50,78 @@ const fadeOutAnimation = keyframes`
   }
 `;
 
-const RoundedRectangle = styled.span`
+const Switch = styled.label`
+    position: relative;
+    display: inline-block;
+    width: ${WIDTH}px;
+    height: ${HEIGHT}px;
+    background-color: ${props => props.toggled ? colors.night.sky : colors.day.sky};
+    border: 6px solid ${props => props.toggled ? colors.night.containerBorder : colors.day.containerBorder};
+    border-radius: ${HEIGHT / 2 + BORDER_WIDTH}px;
+    transition: all ${ANIM_DURATION}s;
+`;
+
+const Input = styled.input`
+    opacity: 0;
+    width: 0;
+    height: 0;
+`;
+
+const Slider = styled.span`
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    transition: ${ANIM_DURATION}s;
+    border-radius: 50%;
+    cursor: pointer;
+`;
+
+const Planet = styled.span`
+    position: absolute;
+    height: ${WIDTH / 2}px;
+    width: ${WIDTH / 2}px;
+    left: ${PLANET_PADDING}px;
+    bottom: ${PLANET_PADDING}px;
+    transition: ${ANIM_DURATION}s;
+    background-color: ${props => props.day ? colors.day.planet.surface : colors.night.planet.surface};
+    border: 6px solid ${props => props.day ? colors.day.planet.border : colors.night.planet.border};
+    border-radius: 50%;
+    transform: ${props => props.day ? 'none' : `translateX(${WIDTH / 2 - PLANET_PADDING * 2}px)`};
+    box-sizing: border-box;
+    overflow: hidden;
+`;
+
+const FreelyPositionedRoundedElement = styled.span`
     top: ${props => props.top}px;
     left: ${props => props.left}px;
     width: ${props => props.width}px;
     height: ${props => props.height}px;
-    border-radius: ${props => props.height / 2}px;
     opacity: ${props => props.visible ? '100' : '0' };
     animation: ${props => props.visible ? fadeInAnimation : fadeOutAnimation} ${ANIM_DURATION / 2}s ease-in-out 0s 1
+    border-radius: ${props => props.height / 2}px;
 
-    background-color: white;
     display: inline-block;
     position: absolute;
 `;
 
-const Cloud = RoundedRectangle;
-const Star = props => <RoundedRectangle {...props} width={props.diameter} height={props.diameter} />;
+const Cloud = styled(FreelyPositionedRoundedElement)`
+  background-color: white;
+`;
+
+const FreelyPositionedRoundElement = props => <FreelyPositionedRoundedElement {...props} width={props.diameter} height={props.diameter} />;
+
+const Crater = styled(FreelyPositionedRoundElement)`
+    border: 3px solid ${colors.night.planet.border};
+    background-color: transparent;
+    border-radius: 50%;
+`;
+
+const Star = styled(FreelyPositionedRoundElement)`
+  background-color: white
+`;
+
 
 const Toggler = () => {
     const [toggled, setToggled] = useState(false);
@@ -112,11 +136,11 @@ const Toggler = () => {
                 toggled={toggled} 
                 onClick={setToggled}
             >
-                <Planet toggled={toggled}>
-                    <Crater toggled={toggled} size={7} top={3} left={5} />
-                    <Crater toggled={toggled} size={6} top={10} left={20} />
-                    <Crater toggled={toggled} size={7} top={3} left={5} />
-                    <Crater toggled={toggled} size={7} top={20} left={5} />
+                <Planet day={!toggled}>
+                    <Crater visible={toggled} diameter={7} top={3} left={5} />
+                    <Crater visible={toggled} diameter={6} top={10} left={20} />
+                    <Crater visible={toggled} diameter={7} top={3} left={5} />
+                    <Crater visible={toggled} diameter={7} top={20} left={5} />
                 </Planet>
             </Slider>
             <Cloud top={20} left={30} visible={!toggled} width={30} height={10} />
