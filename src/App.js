a0e6427eb8c2c76d194515e08e3dab4b789200cd
Lauri Nevanperä <lauri.nevanpera@gmail.com>
2018-12-14 16:38:41 +0200

    tweak styles

--- src/App.js
+++ src/App.js
@@ -7,14 +7,18 @@ const AppWrapper = styled.div`
   justify-content: center;
   align-items: center;
   flex-direction: column;
+
+  ${React.h2} {
+    font-family: 'Roboto Slab', serif;
+  }
 `;
 
 const ToggleWrapper = styled.div`
-  background-color: ${p => p.toggled ? 'rgb(30,30,30)' : 'rgb(234,234,234)'};
+  background-color: ${p => p.toggled ? 'rgb(30,30,30)' : 'rgb(244,244,244)'};
   transition: background-color 0.5s;
   border-radius: 100px;
-  height: 200px;
-  width: 500px;
+  height: 130px;
+  width: 400px;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -33,19 +37,19 @@ const ToggleContainer = ({width, height, borderSize, planetPadding}) => {
 const App = () => (
   <AppWrapper>
     <h2> Vanilla toggle is vanilla </h2>
-    <ToggleContainer width={120} height={68} borderSize={4} planetPadding={4} />
+    <ToggleContainer width={80} height={45} borderSize={3} planetPadding={1} />
 
     <h2> Toggle without borders is toggle without borders </h2>
-    <ToggleContainer width={120} height={68} borderSize={0} planetPadding={4} />
+    <ToggleContainer width={80} height={45} borderSize={0} planetPadding={3} />
 
     <h2> Round toggle is round </h2>
-    <ToggleContainer width={120} height={120} borderSize={0} planetPadding={6} />
+    <ToggleContainer width={80} height={80} borderSize={0} planetPadding={3} />
 
     <h2> Long toggle is loooooong </h2>
-    <ToggleContainer width={350} height={68} borderSize={4} planetPadding={4} />
+    <ToggleContainer width={300} height={45} borderSize={3} planetPadding={2} />
 
-    <h2> Short toggle is short </h2>
-    <ToggleContainer width={120} height={100} borderSize={8} planetPadding={8} />
+    <h2> Stupid looking toggle is stupid </h2>
+    <ToggleContainer width={60} height={45} borderSize={4} planetPadding={8} />
   </AppWrapper>
 );
 
