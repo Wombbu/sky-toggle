0263c2b1a0833966e5def0597e17299852298e9f
Lauri Nevanperä <lauri.nevanpera@gmail.com>
2018-12-14 17:29:43 +0200

    Updates

--- precache-manifest.ac7f1e2d1cb648693b3e7a8754402404.js
+++ precache-manifest.ac7f1e2d1cb648693b3e7a8754402404.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/sky-toggle/static/js/runtime~main.bee046e4.js"
   },
   {
-    "revision": "22fcd4aa6214116d09fa",
-    "url": "/sky-toggle/static/js/main.22fcd4aa.chunk.js"
+    "revision": "9258f2bf045ff77d0606",
+    "url": "/sky-toggle/static/js/main.9258f2bf.chunk.js"
   },
   {
     "revision": "2b99194595f5b3b4765f",
     "url": "/sky-toggle/static/js/1.2b991945.chunk.js"
   },
   {
-    "revision": "7f28be0ee7bbe3a6e9b3bf4147db66ae",
+    "revision": "57ad830c0cb209e84f6db7419a137bcc",
     "url": "/sky-toggle/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/sky-toggle/precache-manifest.3752ab3f72c83e575578477a93fad359.js"
+  "/sky-toggle/precache-manifest.ac7f1e2d1cb648693b3e7a8754402404.js"
 );
 
 workbox.clientsClaim();
